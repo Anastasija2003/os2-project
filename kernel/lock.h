67f49e429f95d6e1c78b61513fc49066e1600e1e
Anastasija2003 <milanovictasa8@gmail.com>
2025-07-11 22:41:58 +0200

    Update lock.h

--- kernel/lock.h
+++ kernel/lock.h
@@ -1,27 +1,36 @@
-//
-// Created by parallels on 7/11/25.
-//
-
 #ifndef LOCK_H
 #define LOCK_H
-#include "defs.h"
 
-void wait(enum RAID_TYPE raid,int diskn,bool reader);
-void signal(enum RAID_TYPE raid,int diskn,bool reader);
-//RAID0
-void wait0(int diskn,bool reader);
-void signal0(int diskn,bool reader);
-//RAID1
-void wait1(int diskn,bool reader);
-void signal1(int diskn,bool reader);
-//RAID4
-void wait4(int diskn,bool reader);
-void signal4(int diskn,bool reader);
-//RAID5
-void wait5(int diskn,bool reader);
-void signal5(int diskn,bool reader);
-//RAID1_0
-void wait0_1(int diskn,bool reader);
-void signal0_1(int diskn,bool reader);
+#include "types.h"
+#include "sleeplock.h"
+#include <stdbool.h>  // za 'bool'
 
-#endif //LOCK_H
+// Glavni API
+void wait(enum RAID_TYPE raid, int diskn, bool reader);
+void signal(enum RAID_TYPE raid, int diskn, bool reader);
+
+// RAID-specific varijante
+void wait0(int diskn, bool reader);
+void signal0(int diskn, bool reader);
+
+void wait1(int diskn, bool reader);
+void signal1(int diskn, bool reader);
+
+void wait4(int diskn, bool reader);
+void signal4(int diskn, bool reader);
+
+void wait5(int diskn, bool reader);
+void signal5(int diskn, bool reader);
+
+void wait0_1(int diskn, bool reader);
+void signal0_1(int diskn, bool reader);
+
+// Struktura za lock info po disku
+struct lock_info {
+    int readers;
+    int writers;
+    struct sleeplock* locks[7];  // do 7 diskova podržano
+};
+
+#endif // LOCK_H
+v
